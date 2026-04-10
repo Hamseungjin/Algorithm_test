d58d8d2d615a9ffa3063f2789d1f7a5da7ccbd75
Ubuntu <ubuntu@ip-172-31-21-136.ap-northeast-2.compute.internal>
2026-04-10 09:25:37 +0900

    original resource push

--- resource/static/js/application-validation.js
+++ resource/static/js/application-validation.js
@@ -340,12 +340,17 @@ const validateSurveyInfo = () => {
 }
 
 const validateFilesInfo = () => {
+    let copyrightFile = $('#copyright-file');
+    notBlank(copyrightFile, "저작권 동의서 파일을 선택하세요.");
 
     let ideaPlanFile = $('#idea-plan-file');
     notBlank(ideaPlanFile, "아이디어 기획서 파일을 선택하세요.");
 
     let ideaSummaryFile = $('#idea-summary-file');
     notBlank(ideaSummaryFile, "아이디어 요약서 파일을 선택하세요.");
+
+    let personalInfoFile = $('#personal-info-file');
+    notBlank(personalInfoFile, "개인정보제공 동의서를 선택하세요.");
 
     // 파일 용량 제한 처리 : notblank 안에서 switch case로 예외처리
 }
